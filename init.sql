fe82068fa2b88606468a533bd4622bea947bafa0
TiredEspressoBean <isherwoc@student.osu.edu>
2025-06-10 18:09:02 -0400

    First full demo version

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE USER readonly_user WITH PASSWORD 'readonly_pw';
+GRANT CONNECT ON DATABASE tracker TO readonly_user;
+GRANT USAGE ON SCHEMA public TO readonly_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly_user;
